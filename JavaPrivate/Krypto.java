9508e2d67af2c1c06a54dfb3522a02a942997ce4
Béla Kádár <bela_kadar@yahoo.com>
2024-11-27 10:22:14 +0100

    Update Krypto.java

--- JavaPrivate/Krypto.java
+++ JavaPrivate/Krypto.java
@@ -7,11 +7,16 @@ public class Krypto {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bitte geben Sie einen Betrag in Euro an: ");
         Double money = scanner.nextDouble();
-
+        convertToXRP(money);
+        convertToXLM(money);
+        scanner.close();
 
     }
     
     public static void convertToXRP(Double eur){
         System.out.println("Der Betrag in XRP ist: "+eur * 1.35);
+    }
+    public ststaic void convertToXLM (Double eur){
+        System.out.println("Der Betrag in XLM ist: "+eur * 0.46)
     }
 }
