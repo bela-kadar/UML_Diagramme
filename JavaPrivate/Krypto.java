57d5bf2fdee6341d3f04289c2c9ea594b0908e9c
Bela Kadar <bela_kadar@yahoo.com>
2024-11-27 10:23:43 +0100

    Java01

--- JavaPrivate/Krypto.java
+++ JavaPrivate/Krypto.java
@@ -16,7 +16,7 @@ public class Krypto {
     public static void convertToXRP(Double eur){
         System.out.println("Der Betrag in XRP ist: "+eur * 1.35);
     }
-    public ststaic void convertToXLM (Double eur){
+    public static void convertToXLM (Double eur){
         System.out.println("Der Betrag in XLM ist: "+eur * 0.46)
     }
 }
