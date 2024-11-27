c5a963bc2aa540a691cc5ecca25afc2e49dc972f
Bela Kadar <bela_kadar@yahoo.com>
2024-11-27 10:14:36 +0100

    Java

---
+++ JavaPrivate/Krypto.java
@@ -0,0 +1,17 @@
+package JavaPrivate;
+
+import java.util.Scanner;
+
+public class Krypto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie einen Betrag in Euro an: ");
+        Double money = scanner.nextDouble();
+
+
+    }
+    
+    public static void convertToXRP(Double eur){
+        System.out.println("Der Betrag in XRP ist: "+eur * 1.35);
+    }
+}
